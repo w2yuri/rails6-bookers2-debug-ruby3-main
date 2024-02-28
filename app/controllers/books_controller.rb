2522fcea4450ecef3b7e038c07a5b8b00f6a2c70
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-02-28 05:19:36 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit]
 
   def show
     @book = Book.find(params[:id])
@@ -49,5 +50,12 @@ class BooksController < ApplicationController
 
   def book_params
   params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
